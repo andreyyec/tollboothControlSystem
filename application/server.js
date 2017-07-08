0c708054fc122f88674c23bf68a29a1811d33623
andreyc <andreyyec@gmail.com>
2017-07-07 18:06:51 -0600

    returning server.js to default state

--- application/server.js
+++ application/server.js
@@ -1,21 +1,9 @@
-/*const 	express = require('express'),
+const 	express = require('express'),
 		expressLayouts = require('express-ejs-layouts'),
 		app = express(),
 		io = require('socket.io')(app),
 		MongoClient = require('mongodb').MongoClient,
-		dbMng = require('./scripts/dbManager.js');*/
-
-
-
-const 	http = require('http'),
-		express = require('express'),
-		expressLayouts = require('express-ejs-layouts'),
-    	app = module.exports.app = express();
-		server = http.createServer(app);
-		io = require('socket.io').listen(server),  //pass a http.Server instance
-		MongoClient = require('mongodb').MongoClient,
 		dbMng = require('./scripts/dbManager.js');
-
 
 //DB manager settings
 let dbManager = new dbMng();
@@ -32,23 +20,6 @@ app.use('/public', express.static('public'));
 app.listen(3000, (req, res) => {
     console.log('listening on 3000')
 });
-
-//Web Sockets settings
-/*io.on('connection', function (socket) {
-  	socket.emit('speak', { hello: 'world' });
-  	
-  	socket.on('listen', function (data) {
-    	console.log(data);
-  	});
-});*/
-
-console.log('Socket IO UP');
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
 
 app.get('/', (req, res) => {
 	let records = dbManager.getTrafficRecordsCount();
@@ -74,15 +45,11 @@ app.get('/settings', (req, res) => {
   	}); 
 });
 
-app.get('/testUri', function(req, res){
-	console.log('REST');
+app.get('*', function(req, res){
+   	res.render('404', {
+    	activeTab : 0,
+		tabTitle: 'Not found error - TCSb',
+	 	mainTitle: '',
+		subTitle: '',
+  	});
 });
-
-// app.get('*', function(req, res){
-//    	res.render('404', {
-//     	activeTab : 0,
-// 		tabTitle: 'Not found error - TCSb',
-// 	 	mainTitle: '',
-// 		subTitle: '',
-//   	});
-// });
